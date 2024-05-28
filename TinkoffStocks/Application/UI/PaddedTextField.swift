7f0c16ceab249b1322bd0ee6f1223dc0d5a37f28
sleepcha <sleepchaa@gmail.com>
2024-05-28 14:58:49 +0700

    added visual state change for `isEnabled`
    added small padding around the text rect when left or right view is present

--- TinkoffStocks/Application/UI/PaddedTextField.swift
+++ TinkoffStocks/Application/UI/PaddedTextField.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class PaddedTextField: UITextField {
     var padding: CGFloat = 16
+    private let textToOverlayViewsPadding: CGFloat = 4
+
+    private var _clearButton: UIView?
+    private var clearButton: UIView? {
+        _clearButton = _clearButton ?? subviews.first(ofType: "_UITextFieldClearButton")
+        return _clearButton
+    }
+
+    override var isEnabled: Bool {
+        didSet {
+            let alphaValue = isEnabled ? 1 : 0.25
+            textColor = textColor?.withAlphaComponent(alphaValue)
+            leftView?.alpha = alphaValue
+            rightView?.alpha = alphaValue
+            clearButton?.alpha = alphaValue
+        }
+    }
 
     override func textRect(forBounds bounds: CGRect) -> CGRect {
         calculateTextRect(super.textRect(forBounds: bounds))
@@ -36,14 +53,18 @@ class PaddedTextField: UITextField {
     }
 
     private func calculateTextRect(_ rect: CGRect) -> CGRect {
-        let leftViewWidth = rect.origin.x - padding
-        let leftPadding = (leftViewWidth > 0 ? 8 : padding)
-        let rightPadding = padding
+        guard rect.size != CGSizeMake(100, 100) else { return rect }
+
+        let isLeftViewVisible = rect.origin.x > padding
+        let isRightViewVisible = bounds.width - rect.width != 0
+        let leftPadding = isLeftViewVisible ? textToOverlayViewsPadding : padding
+        let rightPadding = padding + (isRightViewVisible ? textToOverlayViewsPadding : 0)
+
         return rect.inset(by: UIEdgeInsets(top: 0, left: leftPadding, bottom: 0, right: rightPadding))
     }
 
     private func calculateViewRect(forBounds bounds: CGRect, viewRect: CGRect, isLeftView: Bool = false) -> CGRect {
-        let x = isLeftView ? padding : bounds.size.width - viewRect.width - padding
+        let x = isLeftView ? padding : bounds.width - viewRect.width - padding
         let y = bounds.height / 2 - viewRect.height / 2
         return CGRect(origin: CGPointMake(x, y), size: viewRect.size)
     }
