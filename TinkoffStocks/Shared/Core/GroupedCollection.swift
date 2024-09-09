ed774f20c68e52c67378404b4c869138dd2a95ae
sleepcha <sleepchaa@gmail.com>
2024-09-09 12:42:05 +0700

    changed interfaces of subscripts to accommodate to tableViews

--- TinkoffStocks/Shared/Core/GroupedCollection.swift
+++ TinkoffStocks/Shared/Core/GroupedCollection.swift
@@ -1,45 +1,45 @@
 import Foundation
 
-// MARK: - GroupedCollection
-
 struct GroupedCollection<Group: Hashable, Element> {
-    var groupCount: Int { groups.count }
-    
+    var numberOfGroups: Int { result.count }
+
     private let groupingKey: (Element) -> Group
     private let groupsComparator: KeyPathComparator<Group>
     private let elementsComparator: KeyPathComparator<Element>
 
-    private var collections = [Group: [Element]]()
-    private var groups = [Group]()
+    private var result = [(group: Group, elements: [Element])]()
 
     init(
         _ array: [Element]? = nil,
         groupedBy: @escaping (Element) -> Group,
-        groupsSortedBy: KeyPathComparator<Group>,
-        elementsSortedBy: KeyPathComparator<Element>
+        groupsSortedBy: KeyPath<Group, some Comparable>,
+        groupsOrder: SortOrder = .forward,
+        elementsSortedBy: KeyPath<Element, some Comparable>,
+        elementsOrder: SortOrder = .forward
     ) {
         self.groupingKey = groupedBy
-        self.groupsComparator = groupsSortedBy
-        self.elementsComparator = elementsSortedBy
+        self.groupsComparator = KeyPathComparator(groupsSortedBy, order: groupsOrder)
+        self.elementsComparator = KeyPathComparator(elementsSortedBy, order: elementsOrder)
         if let array { set(with: array) }
     }
 
-    mutating func set(with array: [Element]) {
-        collections = Dictionary(grouping: array, by: groupingKey)
-        groups = collections.keys.sorted(using: groupsComparator)
-
-        for i in collections.values.indices {
-            collections.values[i].sort(using: elementsComparator)
-        }
-    }
-
-    subscript(index: Int) -> (group: Group, elements: [Element]) {
-        let group = groups[index]
-        return (group, collections[group]!)
+    subscript(groupIndex: Int) -> (group: Group, numberOfElements: Int) {
+        (result[groupIndex].group, result[groupIndex].elements.count)
     }
 
     subscript(indexPath: IndexPath) -> Element {
-        let group = groups[indexPath.section]
-        return collections[group]![indexPath.row]
+        let groupIndex = indexPath.section
+        let elementIndex = indexPath.row
+        return result[groupIndex].elements[elementIndex]
+    }
+
+    mutating func set(with array: [Element]) {
+        let collections = Dictionary(grouping: array, by: groupingKey)
+        let groups = collections.keys.sorted(using: groupsComparator)
+
+        // array of tuples sorted by tuple's first item (group)
+        result = groups.map { group in
+            (group, collections[group]!.sorted(using: elementsComparator))
+        }
     }
 }
