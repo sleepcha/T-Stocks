3ad060ab39e7d0d12c115d90cebca26a056a099a
sleepcha <sleepchaa@gmail.com>
2024-09-20 13:40:55 +0700

    moved some local constants to the `C` namespace

--- TinkoffStocks/Application/Constants.swift
+++ TinkoffStocks/Application/Constants.swift
@@ -5,6 +5,8 @@
 //  Created by sleepcha on 8/13/24.
 //
 
+import Foundation
+
 /// A global namespace for constants.
 public enum C {
     public enum Keys {
@@ -15,5 +17,15 @@ public enum C {
         static let keychainService = "com.sleepcha.T-Stocks"
         static let loggerSubsystem = "com.sleepcha.T-Stocks"
         static let appNameHeader = "sleepcha.T-Stocks"
+        static let rubleAsset = "a92e2e25-a698-45cc-a781-167cf465257c"
+    }
+
+    public enum Defaults {
+        static let retryCount = 2
+        static let rateLimitReset: TimeInterval = 60
+    }
+
+    public enum UI {
+        static let standardSpacing: CGFloat = 16
     }
 }
