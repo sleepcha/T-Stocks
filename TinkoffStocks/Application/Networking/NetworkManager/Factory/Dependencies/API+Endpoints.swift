4ad60ae4b5011ab18e53cb3430ffb4b19568cc36
sleepcha <sleepchaa@gmail.com>
2024-09-20 12:40:30 +0700

    added a layer of abstraction, divided `Endpoint` protocol in two
    ...between TInvestAPIClient endpoints and NetworkManager. Now an empty call like `API.getPortfolio` is not accepted by `fetch` method as an argument since it's a simple closure that requires to pass a corresponding request.

---
+++ TinkoffStocks/Application/Networking/NetworkManager/Factory/Dependencies/API+Endpoints.swift
@@ -0,0 +1,35 @@
+//
+//  API+Endpoints.swift
+//  T-Stocks
+//
+//  Created by sleepcha on 8/29/24.
+//
+
+import Foundation
+
+extension API {
+    typealias POSTProvider<Request: Encodable, Response: Decodable> = (Request) -> POST<Request, Response>
+
+    static let getAccounts = TInvestAPIClient.getAccounts.postProvider
+    static let getPortfolio = TInvestAPIClient.getPortfolio.postProvider
+    static let postOrder = TInvestAPIClient.postOrder.postProvider
+    static let getClosePrices = TInvestAPIClient.getClosePrices.postProvider
+    static let getInstrumentBy = TInvestAPIClient.getInstrumentBy.postProvider
+    static let getShareBy = TInvestAPIClient.getShareBy.postProvider
+    static let getBondBy = TInvestAPIClient.getBondBy.postProvider
+    static let getETFBy = TInvestAPIClient.getETFBy.postProvider
+    static let getFutureBy = TInvestAPIClient.getFutureBy.postProvider
+    static let getOptionBy = TInvestAPIClient.getOptionBy.postProvider
+    static let getCurrencyBy = TInvestAPIClient.getCurrencyBy.postProvider
+    static let openSandboxAccount = TInvestAPIClient.openSandboxAccount.postProvider
+    static let closeSandboxAccount = TInvestAPIClient.closeSandboxAccount.postProvider
+    static let sandboxPayIn = TInvestAPIClient.sandboxPayIn.postProvider
+}
+
+private extension TInvestAPIClient.API {
+    var postProvider: API.POSTProvider<Request, Response> {
+        { request in
+            API.POST(path: path, request: request)
+        }
+    }
+}
--- TinkoffStocks/Application/Networking/NetworkManager/Models/Endpoint.swift
+++ TinkoffStocks/Application/Networking/NetworkManager/Models/Endpoint.swift
@@ -5,14 +5,28 @@
 //  Created by sleepcha on 8/17/24.
 //
 
+import Foundation
+
 // MARK: - Endpoint
 
-protocol Endpoint: HTTPRequest {
-    associatedtype Request: Encodable
+protocol Endpoint {
     associatedtype Response: Decodable
+    var path: URL { get }
+}
+
+// MARK: - POSTEndpoint
+
+protocol POSTEndpoint: Endpoint {
+    associatedtype Request: Encodable
+    var request: Request { get }
 }
 
 // MARK: - API
 
-/// A convenient namespace for endpoints; to be extended with static instances of the `Endpoint` conforming type.
-enum API {}
+/// A convenient namespace for endpoints.
+enum API {
+    struct POST<Request: Encodable, Response: Decodable>: POSTEndpoint {
+        let path: URL
+        let request: Request
+    }
+}
