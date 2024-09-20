8a73235b64a191de9a03359ebfe757c2cadbfe0d
sleepcha <sleepchaa@gmail.com>
2024-09-20 15:12:44 +0700

    added a universal datasource model for table/collection-views

---
+++ TinkoffStocks/Shared/Models/DataSource.swift
@@ -0,0 +1,30 @@
+//
+//  DataSource.swift
+//  T-Stocks
+//
+//  Created by sleepcha on 9/8/24.
+//
+
+import Foundation
+
+struct DataSource<Item> {
+    struct Section {
+        let header: String?
+        let items: [Item]
+    }
+
+    var sections: [Section]
+
+    var numberOfSections: Int {
+        sections.count
+    }
+
+    func numberOfItems(in section: Int) -> Int {
+        guard section < sections.count else { return 0 }
+        return sections[section].items.count
+    }
+
+    func item(at indexPath: IndexPath) -> Item {
+        sections[indexPath.section].items[indexPath.row]
+    }
+}
