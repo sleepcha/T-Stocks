93bc67c2dc441e3c73e5160e578489f7724cb5c4
sleepcha <sleepchaa@gmail.com>
2024-09-20 14:17:35 +0700

    added AppFlow as the composition root and the window-holder

--- TinkoffStocks/Application/SceneDelegate.swift
+++ TinkoffStocks/Application/SceneDelegate.swift
@@ -3,26 +3,20 @@
 //  TinkoffStocks
 //
 //  Created by sleepcha on 8/23/23.
-//  
 //
-    
+//
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+// MARK: - SceneDelegate
 
-    var window: UIWindow?
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var appFlow: AppFlow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        window?.backgroundColor = .systemBackground
-        
-        let firstScreenVC = ViewController()
-        firstScreenVC.view.backgroundColor = .systemYellow
-        window?.rootViewController = UINavigationController(rootViewController: firstScreenVC)
-        window?.makeKeyAndVisible()
+
+        appFlow = AppFlow(windowScene: windowScene)
+        appFlow?.start()
     }
 }
