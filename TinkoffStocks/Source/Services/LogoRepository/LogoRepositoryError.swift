59b707d386bd9851a2b19c79535bc396724617ec
sleepcha <sleepchaa@gmail.com>
2024-10-07 15:53:26 +0700

    added taskCancelled case, implemented LocalizedError description

--- TinkoffStocks/Source/Services/LogoRepository/LogoRepositoryError.swift
+++ TinkoffStocks/Source/Services/LogoRepository/LogoRepositoryError.swift
@@ -9,10 +9,24 @@ import Foundation
 
 // MARK: - LogoRepositoryError
 
-enum LogoRepositoryError: Error {
+enum LogoRepositoryError: LocalizedError {
     case invalidURL
+    case taskCancelled
     case networkError
     case serverError
+
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            String(localized: "LogoRepositoryError.invalidURL", defaultValue: "Неверный URL")
+        case .networkError:
+            String(localized: "RepositoryError.networkError")
+        case .serverError:
+            String(localized: "RepositoryError.serverError")
+        case .taskCancelled:
+            String(localized: "RepositoryError.taskCancelled")
+        }
+    }
 }
 
 // MARK: - Error mapping
@@ -20,6 +34,8 @@ enum LogoRepositoryError: Error {
 extension LogoRepositoryError {
     init(httpClientError: HTTPClientError) {
         self = switch httpClientError {
+        case .networkError(let error as URLError) where error.code == .cancelled:
+            .taskCancelled
         case .networkError:
             .networkError
         default:
