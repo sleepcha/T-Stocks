3127613f2b8b4390cc9ebce6957f71b55221c0e1
sleepcha <sleepchaa@gmail.com>
2024-10-24 11:20:09 +0700

    updated to use new AsyncTask version; switched to getting a single `portfolioData` per request

--- TinkoffStocks/Source/Services/PortfolioDataRepository/PortfolioDataRepository.swift
+++ TinkoffStocks/Source/Services/PortfolioDataRepository/PortfolioDataRepository.swift
@@ -2,15 +2,14 @@
 //  PortfolioDataRepository.swift
 //  T-Stocks
 //
-//  Created by sleepcha on 8/18/24.
+//  Created by sleepcha on 10/23/24.
 //
-
-import Foundation
 
 // MARK: - PortfolioDataRepository
 
 protocol PortfolioDataRepository {
-    func getPortfoliosData(accountIDs: [String], completion: @escaping (RepositoryResult<[String: PortfolioData]>) -> Void) -> AsyncTask
+    func getPortfolioData(id: String) -> AsyncTask<PortfolioData, RepositoryError>
+    func getPortfolioData(id: String, completion: @escaping Handler<Result<PortfolioData, RepositoryError>>)
 }
 
 // MARK: - PortfolioDataRepositoryImpl
@@ -22,59 +21,13 @@ final class PortfolioDataRepositoryImpl: PortfolioDataRepository {
         self.networkManager = networkManager
     }
 
-    func getPortfoliosData(accountIDs: [String], completion: @escaping (RepositoryResult<[String: PortfolioData]>) -> Void) -> AsyncTask {
-        guard !accountIDs.isEmpty else {
-            return AsyncTask.empty { completion(.success([:])) }
-        }
-
-        var result = [String: PortfolioData]()
-        let lock = NSLock()
-
-        let tasks = accountIDs.map { id in
-            getPortfolioData(accountID: id) {
-                guard let data = $0.success else { return }
-                lock.withLock { result[id] = data }
-            }
-        }
-
-        return AsyncGroup(tasks, shouldCancelOnError: .always) {
-            if let error = $0.first as? RepositoryError {
-                completion(.failure(error))
-            } else {
-                completion(.success(result))
-            }
-        }
+    func getPortfolioData(id: String) -> AsyncTask<PortfolioData, RepositoryError> {
+        networkManager.fetch(API.getPortfolio(PortfolioRequest(accountId: id)))
+            .map(PortfolioData.init)
+            .mapError(RepositoryError.init)
     }
 
-    private func getPortfolioData(accountID: String, completion: @escaping (RepositoryResult<PortfolioData>) -> Void) -> AsyncTask {
-        networkManager.fetch(
-            API.getPortfolio(PortfolioRequest(accountId: accountID)),
-            completion: { completion($0.map(PortfolioData.init).mapError(RepositoryError.init)) }
-        )
-    }
-}
-
-// MARK: - Model mapping
-
-private extension PortfolioData {
-    init(_ response: PortfolioResponse) {
-        self.init(
-            id: response.accountId,
-            totalAmount: response.totalAmountPortfolio.asDecimal ?? 0,
-            items: response.positions.map(Item.init)
-        )
-    }
-}
-
-private extension PortfolioData.Item {
-    init(_ position: PortfolioPosition) {
-        self.init(
-            id: position.instrumentUid,
-            kind: Kind(rawValue: position.instrumentType.lowercased()) ?? .other,
-            quantity: position.quantity.asDecimal ?? 0,
-            currentPrice: position.currentPrice?.asDecimal ?? 0,
-            averagePrice: position.averagePositionPriceFifo?.asDecimal ?? 0,
-            isBlocked: position.blocked
-        )
+    func getPortfolioData(id: String, completion: @escaping Handler<Result<PortfolioData, RepositoryError>>) {
+        getPortfolioData(id: id).run(completion: completion)
     }
 }
