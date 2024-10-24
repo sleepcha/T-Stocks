d80e2eda848b6a43373e3a28a3e63bdfa5c803fc
sleepcha <sleepchaa@gmail.com>
2024-10-24 10:42:50 +0700

    updated to use new AsyncTask version (no need for completion handlers)

--- TinkoffStocks/Source/Networking/NetworkManager/NetworkManager.swift
+++ TinkoffStocks/Source/Networking/NetworkManager/NetworkManager.swift
@@ -11,10 +11,10 @@ import Foundation
 
 protocol NetworkManager {
     /// Returns a task that calls the completion handler with a cached response if available (and `NetworkManager` is caching) or with the result of a network request.
-    func fetch<T: Endpoint>(_ endpoint: T, retryCount: Int, completion: @escaping (T.Result) -> Void) -> AsyncTask
+    func fetch<T: Endpoint>(_ endpoint: T, retryCount: Int) -> AsyncTask<T.Response, NetworkManagerError>
 
     /// A version of the method with the default `retryCount` value.
-    func fetch<T: Endpoint>(_ endpoint: T, completion: @escaping (T.Result) -> Void) -> AsyncTask
+    func fetch<T: Endpoint>(_ endpoint: T) -> AsyncTask<T.Response, NetworkManagerError>
 
     /// Returns a modified instance that will cache each response with the specified expiry.
     func caching(_ expiry: Expiry) -> NetworkManager
@@ -24,13 +24,9 @@ protocol NetworkManager {
 }
 
 extension NetworkManager {
-    func fetch<T: Endpoint>(_ endpoint: T, completion: @escaping (T.Result) -> Void) -> AsyncTask {
-        fetch(endpoint, retryCount: C.Defaults.retryCount, completion: completion)
+    func fetch<T: Endpoint>(_ endpoint: T) -> AsyncTask<T.Response, NetworkManagerError> {
+        fetch(endpoint, retryCount: C.Defaults.retryCount)
     }
-}
-
-extension Endpoint {
-    typealias Result = Swift.Result<Response, NetworkManagerError>
 }
 
 // MARK: - NetworkManagerImpl
@@ -64,33 +60,30 @@ final class NetworkManagerImpl: NetworkManager {
         self.rateLimitManager = RateLimitManager(dateProvider: dateProvider)
     }
 
-    func fetch<T: Endpoint>(_ endpoint: T, retryCount: Int, completion: @escaping (T.Result) -> Void) -> AsyncTask {
-        AsyncTask(id: "fetch:\(endpoint.path)") { [self] task in
+    func fetch<T: Endpoint>(_ endpoint: T, retryCount: Int) -> AsyncTask<T.Response, NetworkManagerError> {
+        AsyncTask(label: "fetch:\(endpoint.path)") { [self] task in
             let request: HTTPRequest
-
-            let completion = { (result: T.Result) in
-                completion(result)
-                task.done(error: result.failure)
-            }
 
             switch createHTTPRequest(with: endpoint) {
             case .success(let httpRequest):
                 request = httpRequest
             case .failure(let error):
+                #if DEBUG
                 print(error)
-                completion(.failure(.decodingError(error)))
+                #endif
+                task.done(.failure(.codingError(error)))
                 return
             }
 
             if let cacheExpiry, case .success(let response) = client.cached(request, isValid: cacheExpiry.isValid(now())) {
                 let result = response
                     .decoded(as: T.Response.self, using: decoder)
-                    .mapError(NetworkManagerError.decodingError)
-                completion(result)
+                    .mapError(NetworkManagerError.codingError)
+                task.done(result)
                 return
             }
 
-            networkFetch(request, attempts: (left: retryCount, max: retryCount), parentTask: task, completion: completion)
+            networkFetch(request, attempts: (left: retryCount, max: retryCount), parentTask: task)
         }
     }
 
@@ -112,59 +105,58 @@ final class NetworkManagerImpl: NetworkManager {
     private func createHTTPRequest<T: Endpoint>(with endpoint: T) -> Result<HTTPRequest, Error> {
         switch endpoint {
         case let endpoint as any POSTEndpoint:
-            Result { try encoder.encode(endpoint.request) }
-                .map { HTTPRequest(.post, path: endpoint.path, body: $0) }
+            Result {
+                try encoder.encode(endpoint.request)
+            }.map {
+                HTTPRequest(.post, path: endpoint.path, body: $0)
+            }
         default:
             .success(HTTPRequest(.get, path: endpoint.path))
         }
     }
 
-    private func networkFetch<T: Decodable>(
+    private func networkFetch<Response: Decodable>(
         _ httpRequest: HTTPRequest,
         attempts: (left: Int, max: Int),
-        parentTask: AsyncTask,
-        completion: @escaping (Result<T, NetworkManagerError>) -> Void
+        parentTask: AsyncTask<Response, NetworkManagerError>
     ) {
         if let waitTime = rateLimitManager.getResetInterval() {
-            completion(.failure(.tooManyRequests(wait: waitTime)))
+            parentTask.done(.failure(.tooManyRequests(wait: waitTime)))
             return
         }
 
         let cacheMode: CacheMode = (cacheExpiry == nil) ? .disabled : .manual
 
         let dataTask = client.fetchDataTask(httpRequest, cacheMode: cacheMode) { [self] result in
-            guard parentTask.state != .cancelled else {
-                completion(.failure(.taskCancelled))
-                return
-            }
+            guard !parentTask.isCancelled else { return }
 
             let decodedResult = result
                 .mapError(errorMapper)
-                .flatMap { $0.decoded(as: T.self, using: decoder).mapError(NetworkManagerError.decodingError) }
+                .flatMap { $0.decoded(as: Response.self, using: decoder).mapError(NetworkManagerError.codingError) }
 
             switch decodedResult {
             case .failure(let error):
                 handle(
                     error: error,
                     attempts: attempts,
-                    completion: { completion(.failure(error)) },
+                    completion: { parentTask.done(.failure(error)) },
                     retryHandler: {
                         let attemptsLeft = (left: attempts.left - 1, max: attempts.max)
-                        self.networkFetch(httpRequest, attempts: attemptsLeft, parentTask: parentTask, completion: completion)
+                        self.networkFetch(httpRequest, attempts: attemptsLeft, parentTask: parentTask)
                     }
                 )
             case .success(let response):
-                completion(.success(response))
+                parentTask.done(.success(response))
             }
         }
-        parentTask.addCancellationHandler(dataTask.cancel)
+        parentTask.onCancel(dataTask.cancel)
         dataTask.resume()
     }
 
     private func handle(error: NetworkManagerError, attempts: (left: Int, max: Int), completion: VoidHandler, retryHandler: @escaping VoidHandler) {
         switch error {
         case .tooManyRequests(let waitTime):
-            // TODO: enqueue the task to retry after the limit reset
+            // TODO: enqueue the task to retry after the limit reset?
             rateLimitManager.setResetInterval(waitTime)
             completion()
         case .timedOut, .connectionLost:
@@ -200,9 +192,7 @@ private extension Expiry {
             case let .for(period):
                 period.expiration(for: creationDate)
             case let .until(deadline):
-                deadline()
-            case .forever:
-                Date.distantFuture
+                deadline
             }
             return currentDate < expirationDate
         }
