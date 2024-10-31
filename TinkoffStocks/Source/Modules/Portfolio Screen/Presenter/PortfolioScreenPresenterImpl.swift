601e89fbb135aa27f6cdcf9d756275ed6a687819
sleepcha <sleepchaa@gmail.com>
2024-10-31 15:32:58 +0700

    removed redundant method call
    PortfolioItem list is already triggered by `updateAccountSlider()`

--- TinkoffStocks/Source/Modules/Portfolio Screen/Presenter/PortfolioScreenPresenterImpl.swift
+++ TinkoffStocks/Source/Modules/Portfolio Screen/Presenter/PortfolioScreenPresenterImpl.swift
@@ -153,7 +153,6 @@ extension PortfolioScreenPresenterImpl: AccountSliderPresenter {
     func didTapGainPeriodButton() {
         gainPeriod.toggle()
         updateAccountSlider()
-        updatePortfolioItemList()
     }
 
     func didSelectAccount(withID id: String) {
