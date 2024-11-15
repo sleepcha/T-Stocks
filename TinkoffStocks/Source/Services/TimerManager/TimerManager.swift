d792a6f0efe129ffa2aadeb74be7609066934c7d
sleepcha <sleepchaa@gmail.com>
2024-11-15 17:26:07 +0700

    added support of non-repeating timer; replaced `Timer` with `DispatchSourceTimer`

--- TinkoffStocks/Source/Services/TimerManager/TimerManager.swift
+++ TinkoffStocks/Source/Services/TimerManager/TimerManager.swift
@@ -10,7 +10,7 @@ import Foundation
 // MARK: - TimerManager
 
 protocol TimerManager: AnyObject {
-    func startTimer(timeInterval: TimeInterval, tolerance: TimeInterval, action: @escaping VoidHandler)
+    func schedule(timeInterval: TimeInterval, tolerance: TimeInterval, repeats: Bool, action: @escaping VoidHandler)
     func pause()
     func resume()
     func invalidateTimer()
@@ -22,48 +22,61 @@ final class TimerManagerImpl: TimerManager {
     private enum State {
         case invalid
         case running
-        case paused(nextFireDate: Date)
+        case paused
     }
 
-    private weak var timer: Timer?
+    private let queue = DispatchQueue(label: "TimerManager.queue")
+    private var timer: DispatchSourceTimer?
     private var state = State.invalid
 
-    func startTimer(timeInterval: TimeInterval, tolerance: TimeInterval, action: @escaping VoidHandler) {
+    func schedule(timeInterval: TimeInterval, tolerance: TimeInterval, repeats: Bool, action: @escaping VoidHandler) {
         invalidateTimer()
 
-        let timer = Timer(timeInterval: timeInterval, repeats: true) { _ in action() }
-        timer.tolerance = tolerance
-        self.timer = timer
-
-        let runLoop = RunLoop.current
-        runLoop.add(timer, forMode: .common)
-
-        // runs RunLoop if this isn't the main thread
-        if runLoop.currentMode == nil { runLoop.run() }
-
+        let timer = DispatchSource.makeTimerSource(queue: queue)
+        timer.setEventHandler { print("fire the timer @ \(Date())"); action() }
+        timer.schedule(
+            deadline: .now() + timeInterval,
+            repeating: repeats ? .init(timeInterval) : .never,
+            leeway: .init(tolerance)
+        )
+        timer.activate()
         state = .running
+        self.timer = timer
     }
 
     func invalidateTimer() {
-        guard let timer else { return }
-
-        timer.invalidate()
-        state = .invalid
+        queue.sync {
+            timer?.cancel()
+            timer = nil
+            state = .invalid
+        }
     }
 
     func pause() {
-        guard let timer, case .running = state else { return }
-        state = .paused(nextFireDate: timer.fireDate)
-        timer.fireDate = .distantFuture
+        queue.sync {
+            guard let timer, case .running = state else { return }
+            timer.suspend()
+            state = .paused
+        }
     }
 
     func resume() {
-        guard let timer, case .paused(let nextFireDate) = state else { return }
-        timer.fireDate = max(nextFireDate, .now)
-        state = .running
+        queue.sync {
+            guard let timer, case .paused = state else { return }
+            timer.resume()
+            state = .running
+        }
     }
 
     deinit {
         invalidateTimer()
+    }
+}
+
+// MARK: - Helpers
+
+extension DispatchTimeInterval {
+    init(_ timeInterval: TimeInterval) {
+        self = .nanoseconds(Int(timeInterval * 1_000_000_000))
     }
 }
